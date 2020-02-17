ad894c801d81e15d29ff1d3a8564b06e209292b6
ettom <36895504+ettom@users.noreply.github.com>
2020-02-17 02:24:02 +0200

    Refactor Arduino code

--- nano/ws-display.ino
+++ nano/ws-display.ino
@@ -5,111 +5,85 @@
 
 #define WORKSPACES_SENT_DELIMITER 'w'
 #define VISIBLE_SENT_DELIMITER	  'f'
-#define VISIBLE_NOT_FOUND	  '-'
+
+#define SEG_A  0x80
+#define SEG_B  0x40
+#define SEG_G  0x20
+#define SEG_C  0x10
+#define SEG_D  0x08
+#define SEG_E  0x04
+#define SEG_F  0x01
+#define SEG_DP 0x02
+
+#define COM_1 0xfe
+#define COM_2 0xfd
+#define COM_3 0xfb
+#define COM_4 0xf7
+
+const uint8_t decimals[10] = {SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,
+			      SEG_B | SEG_C,
+			      SEG_A | SEG_B | SEG_G | SEG_E | SEG_D,
+			      SEG_A | SEG_B | SEG_G | SEG_C | SEG_D,
+			      SEG_B | SEG_C | SEG_F | SEG_G,
+			      SEG_A | SEG_F | SEG_G | SEG_C | SEG_D,
+			      SEG_A | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G,
+			      SEG_A | SEG_B | SEG_C,
+			      SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G,
+			      SEG_A | SEG_B | SEG_C | SEG_D | SEG_F | SEG_G};
+
+const uint8_t coms[DISPLAY_LENGTH] = {COM_1, COM_2, COM_3, COM_4};
 
 String inputString = "";
 String toPrint = "8888";
 
-char visibleWorkspace = VISIBLE_NOT_FOUND;
+int8_t visibleWorkspace = -1;
 
 unsigned long startMillis;
 unsigned long currentMillis;
-const short refresh_interval = 100;
 
-void lightSegments(char number)
+const uint8_t refreshInterval = 100;
+
+void lightSegments(int number)
 {
-	switch (number) {
-	case '0':
-		PORTD = 0b11011111;
-		PORTC = 0b00000001;
-		break;
-	case '1':
-		PORTD = 0b01010000;
-		PORTC = 0b00000000;
-		break;
-	case '2':
-		PORTD = 0b11101100;
-		PORTC = 0b00000000;
-		break;
-	case '3':
-		PORTD = 0b11111000;
-		PORTC = 0b00000000;
-		break;
-	case '4':
-		PORTD = 0b01110000;
-		PORTC = 0b00000001;
-		break;
-	case '5':
-		PORTD = 0b10111000;
-		PORTC = 0b00000001;
-		break;
-	case '6':
-		PORTD = 0b10111100;
-		PORTC = 0b00000001;
-		break;
-	case '7':
-		PORTD = 0b11010000;
-		PORTC = 0b00000000;
-		break;
-	case '8':
-		PORTD = 0b11111100;
-		PORTC = 0b00000001;
-		break;
-	case '9':
-		PORTD = 0b11111000;
-		PORTC = 0b00000001;
-		break;
-	}
+	PORTD = decimals[number];
+	PORTC = PORTD;
 }
 
 void lightDigit(int index)
 {
-	switch (index) {
-	case 1:
-		PORTB = 0b11111110;
-		break;
-	case 2:
-		PORTB = 0b11111101;
-		break;
-	case 3:
-		PORTB = 0b11111011;
-		break;
-	case 4:
-		PORTB = 0b11110111;
-		break;
-	}
+	PORTB = coms[index];
 }
 
-void lightDotIfVisible(char number)
+void lightDotIfVisible(int number)
 {
 	if (number == visibleWorkspace) {
-		PORTC = PORTC | (1 << 1);
+		PORTC |= SEG_DP;
 	} else {
-		PORTC = PORTC & (1 << 0);
+		PORTC &= ~SEG_DP;
 	}
 }
 
-void printDigit(int index, char number)
+void printDigit(int index, int digit)
 {
-	lightSegments(number);
+	lightSegments(digit);
 	lightDigit(index);
-	lightDotIfVisible(number);
-	delay(5);
+	lightDotIfVisible(digit);
+	delay(2);
 }
 
 void printNumber(String number)
 {
-	while (currentMillis - startMillis < refresh_interval) {
+	while (currentMillis - startMillis < refreshInterval) {
 		currentMillis = millis();
 
-		int positionShift = 1;
+		int positionShift = 0;
 		if (ALIGN_TO_RIGHT) {
-			positionShift = number.length() - (DISPLAY_LENGTH + 1);
+			positionShift = number.length() - (DISPLAY_LENGTH);
 			positionShift = abs(positionShift);
 		}
 
 		for (size_t i = 0; i < number.length(); ++i) {
-			printDigit(i + positionShift, number[i]);
+			printDigit(i + positionShift, number[i] - '0');
 		}
 	}
 
@@ -140,11 +114,6 @@ void setup() // {{{1
 	//-----------1234
 	DDRB = 0b00001111; // pins D11-D8
 
-	// turn everything on
-	PORTD = 0b11111100;
-	PORTC = 0b00000011;
-	PORTB = 0b00000000;
-
 	Serial.begin(9600);
 } // 1}}}
 
@@ -153,16 +122,17 @@ void loop()
 	while (Serial.available() > 0) {
 		char inChar = Serial.read();
 
-		if (inputString.length() < DISPLAY_LENGTH + 1 && isDigit(inChar)) {
+		if (inputString.length() <= DISPLAY_LENGTH && isDigit(inChar)) {
 			inputString += inChar;
 		} else if (inChar == WORKSPACES_SENT_DELIMITER) {
 			toPrint = inputString;
 			inputString = "";
 		} else if (inChar == VISIBLE_SENT_DELIMITER) {
-			visibleWorkspace = inputString[0];
+			visibleWorkspace = inputString[0] - '0';
 			inputString = "";
 		}
 	}
+
 	printNumber(toPrint);
 }
 
